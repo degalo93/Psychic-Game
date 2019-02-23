4eb93c1e5e0a4cad226b8cc99c128b0b372596fe
degalo93 <denisgalo10@gmail.com>
2019-02-22 23:24:45 -0800

    added to read me and worked on js

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Psychic-Game
+
+For this game I attempted to create a game where you the player tries to guess the letter that the program has in mind. 
+
+## The code 
+
+Most of the code that was worked on was on the javascript. I do admit that I had struggled a bit especially with the game logic. At first I had run into some problems with getting the script to show up with the html. 
+
+I had put the `<script type="text/javascript" src="assets/javascript/game.js"></script>` in the head and it would not show any of the work. After sometime I put the script at the bottom of the body and it worked.
+
+### The Javascript 
+
+I had planed out the script following the given prompt. I had planed out what variables I needed and I had a rough idea of the code needed. 
+
+1. I needed to make sure that game would choose a letter at random. 
+    `var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];`
+2. Next to create a series a variables to such as a win , loss, how many guesses left and the players guesses.
+
+3. Next I needed to figure out the logic of the game itself I first started with some if statements. For the first part I wanted to make a code that would follow the rule if guesses reached 0 then your losses would go up and the code would then tell you the letter it was thinking of. 
+
+`if (guessesleft === 0) {
+        losses++;
+        guessesleft = 10;
+        compChoiceText.textContent = "the computer choose: " + compChoice;
+        letterReset.push(random);}`
+
+4. Next I wanted the code that when the player guessed the letter the program choose they would gain a win but if they did not they would lose a guess opportunity.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,16 @@
-/*//variables
-var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-var wins = 0;
-var losses = 0;
-var guessesleft = 10;
-var yourguess = 0;
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var compChoiceText = document.getElementById("compchoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesleftText = document.getElementById("guessesLeft-text");
-var yourguessText = document.getElementById("yourguess-text");
-
-
-document.onkeyup = function(event) {
-
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    if (!(compChoices.indexOf(userGuess) === -1)) {
-
-        var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
-
-
-        if (userGuess === compGuess) {
-            wins++;
-            guessesleft--;
-            yourguess++;
-
-        } else {
-            losses++;
-            guessesleft--;
-            yourguess++;
-        }
-
-        if (guessesleft === 0) {
-            wins = 0;
-            losses = 0;
-            guessesleft = 10;
-            yourguess = 0;
-            resetguess;
-        }
-
-
-
-
-
-        // to display 
-        compChoiceText.textContent = "The computer choose " + compGuess;
-        directionsText.textContent = "";
-        userChoiceText.textContent = "You chose: " + userGuess;
-        winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
-        guessesleftText.textContent = "guesses left: " + guessesleft;
-        yourguessText.textContent = "your guesses: " + yourguess;
-
-    }
-}; */
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var guessesleft = 10;
-var youGuessed = [];
-var letterReset = [];
+var youGuessed = " ";
+var letterReset = "";
 
 var random = letters[Math.floor(Math.random() * letters.length)];
-
+console.log(random);
 var compChoice = random;
+
+
 
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
@@ -87,13 +29,18 @@ document.onkeyup = function(event) {
     if (guessesleft === 0) {
         losses++;
         guessesleft = 10;
-        compChoiceText.textContent = "the computer choose " + compChoice;
+        compChoiceText.textContent = "the computer choose: " + compChoice;
         letterReset.push(random);
 
-    }
+
+    } //else if (userGuess !== random && guessesleft > 0) {
+    //guessesleft--;
+    //youGuessed = youGuessed + userGuess + " ";
+    //}
     if (userGuess === compChoice) {
         wins++;
         youGuessed = [];
+
 
     } else {
         guessesleft--;
@@ -109,10 +56,10 @@ document.onkeyup = function(event) {
 
     directionsText.textContent = " ";
     userChoiceText.textContent = "You chose: " + userGuess;
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    guessesleftText.textContent = "guesses left: " + guessesleft;
-    youguessedText.textContent = "your guesses: " + youGuessed;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesleftText.textContent = "Guesses left: " + guessesleft;
+    youguessedText.textContent = "Your guesses: " + youGuessed;
 
 
 
